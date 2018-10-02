ef6b0a0d5ebfb633f5acfa0bbe5ab5139cf62390
Yosef Yuan <yosef.yuan@6e-group.com>
2018-10-02 20:55:28 +0800

    6-2登录注册页面实现all

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import {
     Redirect,
     Switch
 } from 'react-router-dom'
+import Login from './container/login/login'
+import Register from './container/register/register'
 import reducers from './reducer'
 import config from './config'
 
@@ -19,6 +21,10 @@ const store = createStore(reducers, compose(
 ReactDom.render(
     (<Provider store={store}>
         <BrowserRouter>
+            <div>
+                <Route path='/login' component={Login}></Route>
+                <Route path='/register' component={Register}></Route>
+            </div>
         </BrowserRouter>
     </Provider>),
     document.getElementById('root')
