c120e2587121f07b7f344176c79ce5ebae2fa7ab
Yosef Yuan <yosef.yuan@6e-group.com>
2018-10-02 23:50:21 +0800

    6-4 登录注册-用户信息校验，跳转登录 下一节：登录注册-注册交互实现(05:35)
    下一节
    重新观看

--- src/component/authroute/authroute.js
+++ src/component/authroute/authroute.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import axios from 'axios'
+import { withRouter } from 'react-router-dom'
 
+@withRouter
 class AuthRoute extends React.Component {
     componentDidMount() {
+        const publicList = ['/login', '/register']
+        const pathname = this.props.location.pathname
+        if (publicList.indexOf(pathname) > -1) {
+            return null
+        }
+        console.log(this.props)
         //获取用户信息
         axios.get('user/info')
             .then(res => {
                 if (res.status == 200) {
+                    if (res.data.code == 0) {
+                        // 有登录信息
+                    } else {
+                        this.props.history.push('/login')
+                    }
                     console.log(res.data)
                 }
             })
+
     }
-    render(){
+    render() {
         return <h1>test</h1>
     }
 }
--- src/component/logo/logo.js
+++ src/component/logo/logo.js
@@ -3,6 +3,13 @@ import logoImg from './job.png'
 import './logo.css'
 
 class Logo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.register = this.register.bind(this)
+    }
+    register() {
+        this.props.history.push('/login')
+    }
     render() {
         return <div className="logo-container">
             <img src={logoImg} alt="" />
