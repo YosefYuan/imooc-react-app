179f1c8aa8d3f4c2267b6033fa56d3ab17c8a677
Yosef Yuan <yosef.yuan@6e-group.com>
2018-10-03 20:07:27 +0800

    6-9 登录注册-注册前后端联调

--- server/user.js
+++ server/user.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const Router = express.Router()
+const model = require('./model')
+const User = model.getModule('user')
 
+Router.get('/list', function (req, res) {
+    User.find({}, function (err, doc) {
+        res.json(doc)
+    })
+})
+Router.post('/register', function (req, res) {
+    console.log('body', req.body)
+    const { user, pwd, type } = req.body
+    User.findOne({ user }, function (err, doc) {
+        if (err) {
+            return res.json({ code: 1, msg: '用户名重复' })
+        }
+        User.create({ user, pwd, type }, function (e, doc) {
+            if (e) {
+                return res.json({ code: 1, msg: '后端出错了' })
+            }
+            return res.json({ code: 0 })
+        })
+    })
+})
 Router.get('/info', function (req, res) {
     return res.json({ code: 1 })
 })
