79e6dcd109b92309400622dda68cfc8efe38a51d
yosefyuan <yuanruixiangjet@163.com>
2018-10-04 23:52:57 +0800

    6-12 登录注册-cookie保存登录状态

--- server/user.js
+++ server/user.js
@@ -3,6 +3,10 @@ const utils = require('utility')
 const Router = express.Router()
 const model = require('./model')
 const User = model.getModule('user')
+const _filter = {
+    'pwd': 0,
+    '__v': 0
+}
 
 Router.get('/list', function (req, res) {
     // User.deleteMany({},function(e,d){})
@@ -18,13 +22,14 @@ Router.post('/login', function (req, res) {
     User.findOne({
         user,
         pwd: md5Pwd(pwd)
-    },{'pwd':0}, function (err, doc) {
+    }, _filter, function (err, doc) {
         if (!doc) {
             return res.json({
                 code: 1,
                 msg: '用户名或者密码错误'
             })
         }
+        res.cookie('userid', doc._id)
         return res.json({
             code: 0,
             data: doc
@@ -47,26 +52,59 @@ Router.post('/register', function (req, res) {
                 msg: '用户名重复'
             })
         }
-        User.create({
+        const userModel = new User({
             user,
             type,
             pwd: md5Pwd(pwd)
-        }, function (e, doc) {
+        })
+        userModel.save(function (e, d) {
             if (e) {
                 return res.json({
                     code: 1,
                     msg: '后端出错了'
                 })
             }
+            const {
+                user,
+                type,
+                _id
+            } = d
+            res.cookie('userid', _id)
             return res.json({
-                code: 0
+                code: 0,
+                data: {
+                    user,
+                    type,
+                    _id
+                }
             })
         })
     })
 })
 Router.get('/info', function (req, res) {
-    return res.json({
-        code: 1
+    const {
+        userid
+    } = req.cookies
+    if (!userid) {
+        return res.json({
+            code: 1
+        })
+    }
+    User.findOne({
+        _id: userid
+    }, _filter, function (err, doc) {
+        if (err) {
+            return res.json({
+                code: 1,
+                msg: '后端出错了'
+            })
+        }
+        if (doc) {
+            return res.json({
+                code: 0,
+                data: doc
+            })
+        }
     })
 })
 
--- src/component/authroute/authroute.js
+++ src/component/authroute/authroute.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import axios from 'axios'
-import { withRouter } from 'react-router-dom'
+import {
+    withRouter
+} from 'react-router-dom'
+import {
+    loadData
+} from '../../redux/user.redux'
+import {
+    connect
+} from 'react-redux'
 
 @withRouter
+@connect(
+    null, {
+        loadData
+    }
+)
 class AuthRoute extends React.Component {
     componentDidMount() {
         const publicList = ['/login', '/register']
@@ -10,20 +23,17 @@ class AuthRoute extends React.Component {
         if (publicList.indexOf(pathname) > -1) {
             return null
         }
-        console.log(this.props)
-        //获取用户信息
         axios.get('user/info')
             .then(res => {
                 if (res.status == 200) {
                     if (res.data.code == 0) {
                         // 有登录信息
+                        this.props.loadData(res.data.data)
                     } else {
                         this.props.history.push('/login')
                     }
-                    console.log(res.data)
                 }
             })
-
     }
     render() {
         return null
