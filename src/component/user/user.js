0e5f31955268cd6ee5d981df27936087d89fec07
Yosef Yuan <yosef.yuan@6e-group.com>
2018-10-09 14:58:33 +0800

    9-4 清除cookie登录状态

--- src/component/user/user.js
+++ src/component/user/user.js
@@ -1,14 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Result, List,WhiteSpace } from "antd-mobile";
+import { Result, List, WhiteSpace, Modal } from "antd-mobile";
+import browserCookie from "browser-cookies";
 
 @connect(state => state.user)
 class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logout = this.logout.bind(this);
+  }
+  logout() {
+    const alert = Modal.alert;
+
+    alert("注销", "确认退出登录吗???", [
+      { text: "取消", onPress: () => {} },
+      {
+        text: "确认",
+        onPress: () => {
+          browserCookie.erase("userid");
+          window.location.href = window.location.href;
+        }
+      }
+    ]);
+  }
   render() {
     const props = this.props;
     const Item = List.Item;
     const Brief = Item.Brief;
-    console.log;
     return props.user ? (
       <div>
         <Result
@@ -28,12 +46,17 @@ class User extends React.Component {
             {props.desc.split("\n").map(v => (
               <Brief key={v}>{v}</Brief>
             ))}
-            {props.money ? <Brief>薪资：{props.money}</Brief> : null}
+            {props.money ? (
+              <Brief>
+                薪资：
+                {props.money}
+              </Brief>
+            ) : null}
           </Item>
         </List>
-        <WhiteSpace></WhiteSpace>
+        <WhiteSpace />
         <List>
-            <Item>退出登录</Item>
+          <Item onClick={this.logout}>退出登录</Item>
         </List>
       </div>
     ) : null;
