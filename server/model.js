fab6a208d1b8e0776099e987a3583231c5c2eb6c
Yosef Yuan <yosef.yuan@6e-group.com>
2018-10-10 16:01:10 +0800

    10-4 聊天页面redux链接

--- server/model.js
+++ server/model.js
@@ -1,31 +1,36 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // const DB_URL = "mongodb://127.0.0.1:27017/imooc-chat"
-const DB_URL = 'mongodb://6e_hr:123456xyz@ds261430.mlab.com:61430/6e_hr'
-mongoose.connect(DB_URL)
+const DB_URL = "mongodb://6e_hr:123456xyz@ds261430.mlab.com:61430/6e_hr";
+mongoose.connect(DB_URL);
 
 const models = {
-    user: {
-        'user': { type: String, require: true },
-        'pwd': { type: String, require: true },
-        'type': { type: String, require: true },
-        'avatar': { type: String },
-        'desc': { type: String },
-        'title': { type: String },
-        'company': { type: String },
-        'money': { type: String },
-    },
-    chat: {
-
-    }
-}
+  user: {
+    user: { type: String, require: true },
+    pwd: { type: String, require: true },
+    type: { type: String, require: true },
+    avatar: { type: String },
+    desc: { type: String },
+    title: { type: String },
+    company: { type: String },
+    money: { type: String }
+  },
+  chat: {
+    chatid: { type: String, require: true },
+    from: { type: String, require: true },
+    to: { type: String, require: true },
+    read: { type: Boolean, default: false },
+    content: { type: String, require: true, default: "" },
+    create_time: { type: Number, default: new Date().getTime() }
+  }
+};
 
 for (let m in models) {
-    mongoose.model(m, new mongoose.Schema(models[m]))
+  mongoose.model(m, new mongoose.Schema(models[m]));
 }
 
 module.exports = {
-    getModule: function (name) {
-        return mongoose.model(name)
-    }
-}
+  getModule: function(name) {
+    return mongoose.model(name);
+  }
+};
--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { List, InputItem } from "antd-mobile";
 import io from "socket.io-client";
+import { connect } from "react-redux";
+import { getMsgList } from "../../redux/chat.redux";
+
 const socket = io("ws://localhost:9093");
+
+@connect(
+  state => state,
+  {getMsgList}
+)
 class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = { text: "", msg: [] };
   }
   componentDidMount() {
-    socket.on("recvmsg", data => {
-      console.log(data);
-      this.setState({
-        msg: [...this.state.msg, data.text]
-      });
-    });
+    // socket.on("recvmsg", data => {
+    //   console.log(data);
+    //   this.setState({
+    //     msg: [...this.state.msg, data.text]
+    //   });
+    // });
+    this.props.getMsgList();
   }
   handleSubmit() {
     socket.emit("sendmsg", { text: this.state.text });
