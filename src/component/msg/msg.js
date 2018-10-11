09c3725ec1c1e232f9c8a460d66005edad5c5573
yosef yuan <yuanruixiangjet@163.com>
2018-10-12 01:10:33 +0800

    11-2 聊天列表展示

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,22 +1,48 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { List } from 'antd-mobile'
 
 @connect(
     state => state
 )
 class Msg extends React.Component {
-
+    getLast(arr) {
+        return arr[arr.length - 1]
+    }
     render() {
-        console.log(this.props);
+        const Item = List.Item
+        const Brief = Item.Brief
+        const userid = this.props.user._id
+        const userinfo = this.props.chat.users
         const msgGroup = {}
-
         this.props.chat.chatmsg.forEach(v => {
             msgGroup[v.chatid] = msgGroup[v.chatid] || []
             msgGroup[v.chatid].push(v)
         })
-        console.log(msgGroup)
+        const chatList = Object.values(msgGroup)
+        console.log({ chatList })
         return (
-            <h2>Msg</h2>
+            <div>
+                {chatList.map(v => {
+                    console.log({ v })
+                    const lastItem = this.getLast(v)
+                    const targetid = v[0].from == userid ? v[0].to : v[0].from
+                    if (!userinfo[targetid]) {
+                        return null
+                    }
+                    return (
+                        <List
+                            key={lastItem._id}
+                        >
+                            <Item
+                                thumb={require(`../img/${userinfo[targetid].avatar}.png`)}
+                            >
+                                {lastItem.content}
+                                <Brief>{userinfo[targetid].name}</Brief>
+                            </Item>
+                        </List>)
+                })}
+            </div>
         )
     }
 }
